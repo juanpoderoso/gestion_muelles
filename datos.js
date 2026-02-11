75fe21ce02314c588b757fd2d03220f10071d6bc
juanpoderoso <jpode81@gmail.com>
2026-02-11 21:51:50 +0100

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,106 @@
+var datosMuelles = {
+    "1": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "2": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "3": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "4": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "5": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "6": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "7": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "8": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "9": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "10": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "11": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "12": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "13": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "14": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "15": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "16": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "17": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "18": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "19": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "20": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "21": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "22": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "23": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "24": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "25": {
+        "estado": "Empty",
+        "mat": ""
+    },
+    "26": {
+        "estado": "Empty",
+        "mat": ""
+    }
+};
